8eb316c4e7f34f48777791852b4240a9f5c4ac47
Kevin Klein <7267523+kklein@users.noreply.github.com>
2024-05-28 11:18:08 +0200

    Move data loading to utils. (#122)

--- metalearners/_utils.py
+++ metalearners/_utils.py
@@ -9,6 +9,7 @@ from typing import Union
 
 import numpy as np
 import pandas as pd
+from git_root import git_root
 from sklearn.base import check_array, check_X_y, is_classifier, is_regressor
 from sklearn.ensemble import (
     HistGradientBoostingClassifier,
@@ -320,3 +321,107 @@ def validate_valid_treatment_variant_not_control(
             "pseudo outcomes can only be computed for a treatment variant which isn't "
             "considered to be control."
         )
+
+
+def load_mindset_data() -> tuple[pd.DataFrame, str, str, list[str], list[str]]:
+    # TODO: Optionally make this function work with a URL instead of a file system reference.
+    # That way, we don't need to package the data for someone to be able to use this function.
+    df = pd.read_csv(git_root("data/learning_mindset.zip"))
+    outcome_column = "achievement_score"
+    treatment_column = "intervention"
+    feature_columns = [
+        column
+        for column in df.columns
+        if column not in [outcome_column, treatment_column]
+    ]
+    categorical_feature_columns = [
+        "ethnicity",
+        "gender",
+        "frst_in_family",  # spellchecker:disable-line
+        "school_urbanicity",
+        "schoolid",
+    ]
+    # Note that explicitly setting the dtype of these features to category
+    # allows both lightgbm as well as shap plots to
+    # 1. Operate on features which are not of type int, bool or float
+    # 2. Correctly interpret categoricals with int values to be
+    #    interpreted as categoricals, as compared to ordinals/numericals.
+    for categorical_feature_column in categorical_feature_columns:
+        df[categorical_feature_column] = df[categorical_feature_column].astype(
+            "category"
+        )
+    return (
+        df,
+        outcome_column,
+        treatment_column,
+        feature_columns,
+        categorical_feature_columns,
+    )
+
+
+def load_twins_data(
+    rng: np.random.Generator,
+) -> tuple[pd.DataFrame, str, str, list[str], list[str], str]:
+    # TODO: Optionally make this function work with a URL instead of a file system reference.
+    # That way, we don't need to package the data for someone to be able to use this function.
+    df = pd.read_csv(git_root("data/twins.zip"))
+    drop_columns = [
+        "bord",
+        "brstate_reg",
+        "stoccfipb_reg",
+        "mplbir_reg",
+        "infant_id",
+        "wt",
+    ]
+    # We remove wt (weight) and bord (birth order) as they are different for each twin.
+    # We remove _reg variables as they are already represented by the corresponding
+    # variable without _reg and this new only groups them in bigger regions.
+    # We remove infant_id as it's a unique identifier for each infant.
+    df = df.drop(drop_columns, axis=1)
+    outcome_column = "outcome"
+    treatment_column = "treatment"
+    true_cate_column = "true_cate"
+    feature_columns = [
+        column
+        for column in df.columns
+        if column not in [outcome_column, treatment_column]
+    ]
+    assert len(feature_columns) == 45
+
+    ordinary_feature_columns = [
+        "dlivord_min",
+        "dtotord_min",
+    ]
+    categorical_feature_columns = [
+        column for column in feature_columns if column not in ordinary_feature_columns
+    ]
+    for categorical_feature_column in categorical_feature_columns:
+        df[categorical_feature_column] = df[categorical_feature_column].astype(
+            "category"
+        )
+
+    n_twins_pairs = df.shape[0] // 2
+    chosen_twin = rng.binomial(n=1, p=0.3, size=n_twins_pairs)
+
+    selected_rows = []
+    for i in range(0, len(df), 2):
+        pair_idx = i // 2
+        selected_row_idx = i + chosen_twin[pair_idx]
+        selected_rows.append(selected_row_idx)
+
+    chosen_df = df.iloc[selected_rows].reset_index(drop=True)
+
+    mu_0 = df[df[treatment_column] == 0][outcome_column].reset_index(drop=True)
+    mu_1 = df[df[treatment_column] == 1][outcome_column].reset_index(drop=True)
+    chosen_df["mu_0"] = mu_0
+    chosen_df["mu_1"] = mu_1
+    chosen_df[true_cate_column] = mu_1 - mu_0
+
+    return (
+        chosen_df,
+        outcome_column,
+        treatment_column,
+        feature_columns,
+        categorical_feature_columns,
+        true_cate_column,
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,9 @@
 
 
 import numpy as np
-import pandas as pd
 import pytest
-from git_root import git_root
 
-from metalearners._utils import get_linear_dimension
+from metalearners._utils import get_linear_dimension, load_mindset_data, load_twins_data
 from metalearners.data_generation import (
     compute_experiment_outputs,
     generate_covariates,
@@ -73,92 +71,19 @@ def rng():
 
 @pytest.fixture(scope="session")
 def mindset_data():
-    df = pd.read_csv(git_root("data/learning_mindset.zip"))
-    outcome_column = "achievement_score"
-    treatment_column = "intervention"
-    feature_columns = [
-        column
-        for column in df.columns
-        if column not in [outcome_column, treatment_column]
-    ]
-    categorical_feature_columns = [
-        "ethnicity",
-        "gender",
-        "frst_in_family",  # spellchecker:disable-line
-        "school_urbanicity",
-        "schoolid",
-    ]
-    # Note that explicitly setting the dtype of these features to category
-    # allows both lightgbm as well as shap plots to
-    # 1. Operate on features which are not of type int, bool or float
-    # 2. Correctly interpret categoricals with int values to be
-    #    interpreted as categoricals, as compared to ordinals/numericals.
-    for categorical_feature_column in categorical_feature_columns:
-        df[categorical_feature_column] = df[categorical_feature_column].astype(
-            "category"
-        )
-    return (
-        df,
-        outcome_column,
-        treatment_column,
-        feature_columns,
-        categorical_feature_columns,
-    )
+    return load_mindset_data()
 
 
 @pytest.fixture(scope="function")
 def twins_data(rng):
-    df = pd.read_csv(git_root("data/twins.zip"))
-    drop_columns = [
-        "bord",
-        "brstate_reg",
-        "stoccfipb_reg",
-        "mplbir_reg",
-        "infant_id",
-        "wt",
-    ]
-    # We remove wt (weight) and bord (birth order) as they are different for each twin.
-    # We remove _reg variables as they are already represented by the corresponding
-    # variable without _reg and this new only groups them in bigger regions.
-    # We remove infant_id as it's a unique identifier for each infant.
-    df = df.drop(drop_columns, axis=1)
-    outcome_column = "outcome"
-    treatment_column = "treatment"
-    feature_columns = [
-        column
-        for column in df.columns
-        if column not in [outcome_column, treatment_column]
-    ]
-    assert len(feature_columns) == 45
-
-    ordinary_feature_columns = [
-        "dlivord_min",
-        "dtotord_min",
-    ]
-    categorical_feature_columns = [
-        column for column in feature_columns if column not in ordinary_feature_columns
-    ]
-    for categorical_feature_column in categorical_feature_columns:
-        df[categorical_feature_column] = df[categorical_feature_column].astype(
-            "category"
-        )
-
-    n_twins_pairs = df.shape[0] // 2
-    chosen_twin = rng.binomial(n=1, p=0.3, size=n_twins_pairs)
-
-    selected_rows = []
-    for i in range(0, len(df), 2):
-        pair_idx = i // 2
-        selected_row_idx = i + chosen_twin[pair_idx]
-        selected_rows.append(selected_row_idx)
-
-    chosen_df = df.iloc[selected_rows].reset_index(drop=True)
-
-    mu_0 = df[df[treatment_column] == 0][outcome_column].reset_index(drop=True)
-    mu_1 = df[df[treatment_column] == 1][outcome_column].reset_index(drop=True)
-    chosen_df["mu_0"] = mu_0
-    chosen_df["mu_1"] = mu_1
-
+    (
+        chosen_df,
+        outcome_column,
+        treatment_column,
+        feature_columns,
+        categorical_feature_columns,
+        _,
+    ) = load_twins_data(rng)
     return (
         chosen_df,
         outcome_column,
