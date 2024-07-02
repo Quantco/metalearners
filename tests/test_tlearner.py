9c4c0fba01d27e89044267dce09273477b302799
Francesc Martí Escofet <francesc.marti.escofet@quantco.com>
2024-07-02 14:19:31 +0200

    Add test for categorical features

--- tests/test_tlearner.py
+++ tests/test_tlearner.py
@@ -5,6 +5,7 @@ from functools import partial
 
 import numpy as np
 import onnxruntime as rt
+import pandas as pd
 import pytest
 from lightgbm import LGBMClassifier, LGBMRegressor
 from onnxmltools import convert_lightgbm, convert_xgboost
@@ -74,6 +75,7 @@ from sklearn.tree import (
 from xgboost import XGBClassifier, XGBRegressor
 
 from metalearners import TLearner
+from metalearners._typing import Params
 from metalearners.metalearner import VARIANT_OUTCOME_MODEL
 
 all_sklearn_classifiers = [
@@ -169,17 +171,51 @@ def test_tlearner_onnx(nuisance_model_factory, onnx_converter, is_classification
     if nuisance_model_factory == QuadraticDiscriminantAnalysis:
         # TODO: investigate the cause why the assertion fails
         pytest.skip()
+
+    supports_categoricals = nuisance_model_factory in [
+        LGBMClassifier,
+        LGBMRegressor,
+        # convert_sklearn does not support categoricals https://github.com/onnx/sklearn-onnx/issues/1051
+        # HistGradientBoostingClassifier,
+        # HistGradientBoostingRegressor,
+        # convert_xgboost does not support categoricals https://github.com/onnx/onnxmltools/issues/469#issuecomment-1993880910
+        # XGBClassifier,
+        # XGBRegressor,
+    ]
+
+    nuisance_model_params: Params | None
     if nuisance_model_factory in [RadiusNeighborsClassifier, RadiusNeighborsRegressor]:
         nuisance_model_params = {"radius": 10}
+    elif nuisance_model_factory in [
+        HistGradientBoostingClassifier,
+        HistGradientBoostingRegressor,
+    ]:
+        # This is unnecessary for now but if at some point convert_sklearn supports categoricals this is needed
+        nuisance_model_params = {"categorical_features": "from_dtype"}
+    elif nuisance_model_factory in [XGBClassifier, XGBRegressor]:
+        # This is unnecessary for now but if at some point convert_xgboost supports categoricals this is needed
+        nuisance_model_params = {"enable_categorical": True}
     else:
         nuisance_model_params = None
     n_samples = 300
-    n_features = 10
+    n_numerical_features = 5
     n_variants = 3
     n_classes = (
         2 if nuisance_model_factory == GaussianProcessClassifier else 3
     )  # convert_sklearn only supports binary classification with GaussianProcessClassifier
-    X = rng.standard_normal((n_samples, n_features))
+    X = rng.standard_normal((n_samples, n_numerical_features))
+    if supports_categoricals:
+        n_categorical_features = 2
+        X = pd.DataFrame(X)
+        X[n_numerical_features] = pd.Series(
+            rng.integers(10, 13, n_samples), dtype="category"
+        )  # not start at 0
+        X[n_numerical_features + 1] = pd.Series(
+            rng.choice([-5, 4, -10, -32], size=n_samples), dtype="category"
+        )  # not consecutive
+    else:
+        n_categorical_features = 0
+
     if is_classification:
         y = rng.integers(0, n_classes, size=n_samples)
     else:
@@ -199,7 +235,15 @@ def test_tlearner_onnx(nuisance_model_factory, onnx_converter, is_classification
     for tv in range(n_variants):
         model = ml._nuisance_models[VARIANT_OUTCOME_MODEL][tv]._overall_estimator
         onnx_model = onnx_converter(
-            model, initial_types=[("X", FloatTensorType([None, n_features]))]
+            model,
+            initial_types=[
+                (
+                    "X",
+                    FloatTensorType(
+                        [None, n_numerical_features + n_categorical_features]
+                    ),
+                )
+            ],
         )
         onnx_models.append(onnx_model)
 
@@ -208,8 +252,17 @@ def test_tlearner_onnx(nuisance_model_factory, onnx_converter, is_classification
         final.SerializeToString(), providers=rt.get_available_providers()
     )
 
+    if supports_categoricals:
+        onnx_X = X.to_numpy(np.float32)
+        # This is needed for categoricals as LGBM uses the categorical codes, when
+        # other implementations support categoricals this may need to be changed
+        onnx_X[:, n_numerical_features] = X[n_numerical_features].cat.codes
+        onnx_X[:, n_numerical_features + 1] = X[n_numerical_features + 1].cat.codes
+    else:
+        onnx_X = X.astype(np.float32)
+
     pred_onx = sess.run(
         ["tau"],
-        {"input": X.astype(np.float32)},
+        {"input": onnx_X},
     )
-    np.testing.assert_allclose(ml.predict(X, True, "overall"), pred_onx[0], atol=1e-5)
+    np.testing.assert_allclose(ml.predict(X, True, "overall"), pred_onx[0], atol=1e-4)
