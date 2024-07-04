a7be0cd4222c121bb397caf6449526e10580fb70
Francesc Martí Escofet <francesc.marti.escofet@quantco.com>
2024-07-04 10:24:54 +0200

    Add text about reusage in docs

--- metalearners/grid_search.py
+++ metalearners/grid_search.py
@@ -122,11 +122,14 @@ class MetaLearnerGridSearch:
     Importantly, ``random_state`` must be passed through the ``random_state`` parameter
     and not through ``metalearner_params``.
 
-    ``base_learner_grid`` keys should be the names of all the base models contained in the :class:`~metalearners.metalearners.MetaLearner`
+    ``base_learner_grid`` keys should be the names of the needed base models contained in the :class:`~metalearners.metalearners.MetaLearner`
     defined by ``metalearner_factory``, for information about this names check
     :meth:`~metalearners.metalearner.MetaLearner.nuisance_model_specifications` and
     :meth:`~metalearners.metalearner.MetaLearner.treatment_model_specifications`. The
     values should be sequences of model factories.
+
+    If models are reused, they should be passed through ``metalearner_params`` and they
+    should not be in ``base_learner_grid``.
 
     ``param_grid`` should contain the parameters grid for each type of model used by the
     base learners defined in ``base_learner_grid``. The keys should be strings with the
