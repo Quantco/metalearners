6df7afa3f52f6dd78305a36812e17fd87cb66125
Francesc Martí Escofet <francesc.marti.escofet@quantco.com>
2024-07-10 15:32:58 +0200

    Add errors in outcome validation and tests

--- metalearners/metalearner.py
+++ metalearners/metalearner.py
@@ -317,7 +317,7 @@ class MetaLearner(ABC):
                 f"Yet we found the values {set(np.unique(w))}."
             )
 
-    def _validate_outcome(self, y: Vector) -> None:
+    def _validate_outcome(self, y: Vector, w: Vector) -> None:
         if (
             self.is_classification
             and not self._supports_multi_class()
@@ -327,6 +327,17 @@ class MetaLearner(ABC):
                 f"{self.__class__.__name__} does not support multiclass classification."
                 f" Yet we found {len(np.unique(y))} classes."
             )
+        if self.is_classification:
+            classes_0 = set(np.unique(y[w == 0]))
+            for tv in range(self.n_variants):
+                if set(np.unique(y[w == tv])) != classes_0:
+                    raise ValueError(
+                        "Not all variants have the same classes. Please check your data."
+                    )
+            if len(classes_0) == 1:
+                raise ValueError(
+                    "There is only one class present in the outcome. Please check your data."
+                )
 
     def _validate_models(self) -> None:
         """Validate that the base models are appropriate.
