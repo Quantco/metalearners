0506c8c80776287de0c20a168140c0e029d52e38
kklein <kevin.klein@quantco.com>
2024-08-27 11:10:57 +0200

    Bring back the quotes.

--- nb-convert.sh
+++ nb-convert.sh
@@ -4,5 +4,5 @@ set -e
 
 jupyter nbconvert --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no-convert --to script docs/examples/*.ipynb &&
 for file in docs/examples/*.txt;
-do mv -- $file ${file%.txt}.py;
+do mv -- "$file" "${file%.txt}.py";
 done
