993e5cd139820da2d5c6c7c0e4750173dd69836b
Thomas Marwitz <thomasmarwitz3@gmail.com>
2024-12-13 11:59:30 +0100

    Fix bash for loop with default option

--- nb-convert.sh
+++ nb-convert.sh
@@ -3,6 +3,7 @@
 set -e
 
 jupyter nbconvert --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no-convert --to script docs/examples/*.ipynb &&
+shopt -s nullglob && # Prevents the loop from running if there are no .txt files
 for file in docs/examples/*.txt;
 do mv -- "$file" "${file%.txt}.py";
 done
