81b8f90c856d1576f6e454cae03a471799facc5b
thomasmarwitz <thomasmarwitz3@gmail.com>
2025-02-26 12:35:12 +0100

    Removing log

--- docs/javascripts/external_links.js
+++ docs/javascripts/external_links.js
@@ -3,5 +3,4 @@ document.addEventListener("DOMContentLoaded", function () {
     Array.from(document.links)
         .filter(link => link.hostname != window.location.hostname)
         .forEach(link => link.target = '_blank');
-    console.log(`${document.links.length} External links will be opened in a new tab after clicking.`);
 });
