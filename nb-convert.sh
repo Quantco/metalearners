8de46a47146d85b14c24be8c4c402bae427cd405
thomasmarwitz <thomasmarwitz3@gmail.com>
2025-02-26 12:43:51 +0100

    FUrhter bash improvements

--- nb-convert.sh
+++ nb-convert.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 jupyter nbconvert --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags no-convert --to script docs/examples/*.ipynb &&
-shopt -s nullglob && # Prevents the loop from running if there are no .txt files
+shopt -s nullglob # Prevents the loop from running if there are no .txt files
 for file in docs/examples/*.txt;
 do mv -- "$file" "${file%.txt}.py";
 done
