e32b1fd0551460304248e61ad803a697dc610069
thomasmarwitz <thomasmarwitz3@gmail.com>
2025-02-26 11:46:08 +0100

    Add reference to link snippet

--- docs/javascripts/external_links.js
+++ docs/javascripts/external_links.js
@@ -1,3 +1,4 @@
+// Modified from https://stackoverflow.com/a/4425214
 document.addEventListener("DOMContentLoaded", function () {
     Array.from(document.links)
         .filter(link => link.hostname != window.location.hostname)
