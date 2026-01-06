1d38317ee828d9878cbbc59a9f908254c2194bec
kklein <kevin.klein@quantco.com>
2026-01-06 09:44:46 +0100

    Update pixi lock

--- data/download_twins.py
+++ data/download_twins.py
@@ -62,7 +62,7 @@ lighter_columns = [
     "infant_id_0",
     "dlivord_min",
     "dtotord_min",
-    "bord_0",
+    "bord_0",  # spellchecker:disable-line
     "brstate_reg",
     "stoccfipb_reg",
     "mplbir_reg",
@@ -114,7 +114,7 @@ heavier_columns = [
     "infant_id_1",
     "dlivord_min",
     "dtotord_min",
-    "bord_1",
+    "bord_1",  # spellchecker:disable-line
     "brstate_reg",
     "stoccfipb_reg",
     "mplbir_reg",
@@ -193,7 +193,7 @@ cols = [
     "infant_id",
     "dlivord_min",
     "dtotord_min",
-    "bord",
+    "bord",  # spellchecker:disable-line
     "brstate_reg",
     "stoccfipb_reg",
     "mplbir_reg",
